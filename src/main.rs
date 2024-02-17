c36ffcc1f61b784a912e837b887368a8fae35a4a
lumynou5 <lumynou5.tw@gmail.com>
2024-02-17 12:44:36 +0800

    feat: Remove err from error messages

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod config;
 use crate::config::Config;
 use clap::{Parser, Subcommand};
 use std::fs::{create_dir_all, remove_dir_all, File, OpenOptions};
-use std::io::{BufReader, BufWriter};
+use std::io::{BufReader, BufWriter, ErrorKind};
 use std::path::Path;
 use std::process::ExitCode;
 
@@ -25,16 +25,22 @@ fn main() -> ExitCode {
             let config_file =
                 match File::open("brushdown.yaml").or_else(|_| File::open("brushdown.yml")) {
                     Ok(v) => v,
-                    Err(err) => {
-                        eprintln!("Failed to open the configuration file: {}.", err);
+                    Err(err) if err.kind() == ErrorKind::NotFound => {
+                        eprintln!(
+                            "Configuration file is not found.  Run `brushdown new` to create one."
+                        );
+                        return ExitCode::FAILURE;
+                    }
+                    Err(_) => {
+                        eprintln!("Failed to open the configuration file.");
                         return ExitCode::FAILURE;
                     }
                 };
             let config_reader = BufReader::new(config_file);
             let config = Config::from_raw(match serde_yaml::from_reader(config_reader) {
                 Ok(v) => v,
-                Err(err) => {
-                    eprintln!("Failed to parse the configuration file: {}.", err);
+                Err(_) => {
+                    eprintln!("Failed to parse the configuration file.");
                     return ExitCode::FAILURE;
                 }
             });
@@ -57,20 +63,18 @@ fn main() -> ExitCode {
                 return ExitCode::FAILURE;
             }
             if config.dest.exists() {
-                if let Err(err) = remove_dir_all(&config.dest) {
+                if let Err(_) = remove_dir_all(&config.dest) {
                     eprintln!(
-                        "Failed to clear the destination directory `{}`: {}.",
-                        config.dest.display(),
-                        err
+                        "Failed to clear the destination directory `{}`.",
+                        config.dest.display()
                     );
                     return ExitCode::FAILURE;
                 }
             }
-            if let Err(err) = create_dir_all(&config.dest) {
+            if let Err(_) = create_dir_all(&config.dest) {
                 eprintln!(
-                    "Failed to create the destination directory `{}`: {}.",
-                    config.dest.display(),
-                    err
+                    "Failed to create the destination directory `{}`.",
+                    config.dest.display()
                 );
                 return ExitCode::FAILURE;
             }
@@ -89,14 +93,14 @@ fn main() -> ExitCode {
                     Ok(file) => {
                         match serde_yaml::to_writer(BufWriter::new(file), &Config::default()) {
                             Ok(_) => ExitCode::SUCCESS,
-                            Err(err) => {
-                                eprintln!("Failed to write to the configuration file: {}.", err);
+                            Err(_) => {
+                                eprintln!("Failed to write to the configuration file");
                                 ExitCode::FAILURE
                             }
                         }
                     }
-                    Err(err) => {
-                        eprintln!("Failed to create a configuration file: {}.", err);
+                    Err(_) => {
+                        eprintln!("Failed to create a configuration file.");
                         ExitCode::FAILURE
                     }
                 }
