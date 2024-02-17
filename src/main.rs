649ecce6f3a3f37ed359325cb28d3d65d7e9c4de
lumynou5 <lumynou5.tw@gmail.com>
2024-02-17 15:29:54 +0800

    refactor: Extract subcommands to functions

--- src/main.rs
+++ src/main.rs
@@ -18,85 +18,75 @@ enum Command {
     New,
 }
 
+fn none() -> ExitCode {
+    let config_file = match File::open("brushdown.yaml").or_else(|_| File::open("brushdown.yml")) {
+        Ok(v) => v,
+        Err(err) if err.kind() == ErrorKind::NotFound => {
+            eprintln!("Configuration file is not found.  Run `brushdown new` to create one.");
+            return ExitCode::FAILURE;
+        }
+        Err(_) => {
+            eprintln!("Failed to open the configuration file.");
+            return ExitCode::FAILURE;
+        }
+    };
+    let config_reader = BufReader::new(config_file);
+    let config = Config::from_raw(match serde_yaml::from_reader(config_reader) {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("Failed to parse the configuration file.");
+            return ExitCode::FAILURE;
+        }
+    });
+    if !config.src.is_dir() || config.src.is_symlink() {
+        eprintln!("The source path `{}` is not a directory.", config.src.display());
+        return ExitCode::FAILURE;
+    }
+    if config.src == config.dest {
+        eprintln!("The source directory and the destination directory are same.");
+        return ExitCode::FAILURE;
+    }
+    if (config.dest.exists() && !config.dest.is_dir()) || config.dest.is_symlink() {
+        eprintln!("The destination path `{}` exists and is not a directory.", config.dest.display());
+        return ExitCode::FAILURE;
+    }
+    if config.dest.exists() {
+        if let Err(_) = remove_dir_all(&config.dest) {
+            eprintln!("Failed to clear the destination directory `{}`.", config.dest.display());
+            return ExitCode::FAILURE;
+        }
+    }
+    if let Err(_) = create_dir_all(&config.dest) {
+        eprintln!("Failed to create the destination directory `{}`.", config.dest.display());
+        return ExitCode::FAILURE;
+    }
+    ExitCode::SUCCESS
+}
+
+fn new() -> ExitCode {
+    if Path::new("brushdown.yaml").exists() {
+        eprintln!("A configuration file exists already, please remove it first.");
+        return ExitCode::FAILURE;
+    }
+    let Ok(file) = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open("brushdown.yaml")
+    else {
+        eprintln!("Failed to create a configuration file.");
+        return ExitCode::FAILURE;
+    };
+    let Ok(_) = serde_yaml::to_writer(BufWriter::new(file), &Config::default()) else {
+        eprintln!("Failed to write to the configuration file");
+        return ExitCode::FAILURE;
+    };
+    ExitCode::SUCCESS
+}
+
 fn main() -> ExitCode {
     let cli = Cli::parse();
     match cli.subcommand {
-        None => {
-            let config_file =
-                match File::open("brushdown.yaml").or_else(|_| File::open("brushdown.yml")) {
-                    Ok(v) => v,
-                    Err(err) if err.kind() == ErrorKind::NotFound => {
-                        eprintln!(
-                            "Configuration file is not found.  Run `brushdown new` to create one."
-                        );
-                        return ExitCode::FAILURE;
-                    }
-                    Err(_) => {
-                        eprintln!("Failed to open the configuration file.");
-                        return ExitCode::FAILURE;
-                    }
-                };
-            let config_reader = BufReader::new(config_file);
-            let config = Config::from_raw(match serde_yaml::from_reader(config_reader) {
-                Ok(v) => v,
-                Err(_) => {
-                    eprintln!("Failed to parse the configuration file.");
-                    return ExitCode::FAILURE;
-                }
-            });
-            if !config.src.is_dir() || config.src.is_symlink() {
-                eprintln!(
-                    "The source path `{}` is not a directory.",
-                    config.src.display()
-                );
-                return ExitCode::FAILURE;
-            }
-            if config.src == config.dest {
-                eprintln!("The source directory and the destination directory are same.");
-                return ExitCode::FAILURE;
-            }
-            if (config.dest.exists() && !config.dest.is_dir()) || config.dest.is_symlink() {
-                eprintln!(
-                    "The destination path `{}` exists and is not a directory.",
-                    config.dest.display()
-                );
-                return ExitCode::FAILURE;
-            }
-            if config.dest.exists() {
-                if let Err(_) = remove_dir_all(&config.dest) {
-                    eprintln!(
-                        "Failed to clear the destination directory `{}`.",
-                        config.dest.display()
-                    );
-                    return ExitCode::FAILURE;
-                }
-            }
-            if let Err(_) = create_dir_all(&config.dest) {
-                eprintln!(
-                    "Failed to create the destination directory `{}`.",
-                    config.dest.display()
-                );
-                return ExitCode::FAILURE;
-            }
-            ExitCode::SUCCESS
-        }
-        Some(Command::New) => {
-            if Path::new("brushdown.yaml").exists() {
-                eprintln!("A configuration file exists already, please remove it first.");
-                return ExitCode::FAILURE;
-            }
-            let Ok(file) = OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open("brushdown.yaml") else {
-                eprintln!("Failed to create a configuration file.");
-                return ExitCode::FAILURE;
-            };
-            let Ok(_) = serde_yaml::to_writer(BufWriter::new(file), &Config::default()) else {
-                eprintln!("Failed to write to the configuration file");
-                return ExitCode::FAILURE;
-            };
-            ExitCode::SUCCESS
-        }
+        None => none(),
+        Some(Command::New) => new(),
     }
 }
